7c34372c6ab31679b8f833248798f36577275957
Ajinkya Kamble <ch19b058@smail.iitm.ac.in>
2021-05-12 17:36:39 +0530

    Added Noble Integer

---
+++ Noble-integer(interviewbit).cpp
@@ -0,0 +1,11 @@
+int Solution::solve(vector<int> &A) {
+    
+    sort(A.begin(),A.end());
+    
+    for(int i=0;i<A.size();++i){
+        if(A[i]==A.size()-i-1 && (i==A.size()-1 || A[i]!=A[i+1])) {
+            return 1;
+        }
+    }
+    return -1;
+}
